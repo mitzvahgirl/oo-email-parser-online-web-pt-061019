feb5a7b1a260dbfdb8fb6adb6693ddcbf9261578
Amy Fogelman <abfnj1@gmail.com>
2019-07-17 20:42:43 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,13 +5,13 @@ class EmailParser
     @emails = emails
   end 
 
-  def parse 
-    puts split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
-end 
+  def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
+end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
 parser1 = EmailParser.new(emails1)
@@ -20,4 +20,3 @@ parser1.parse
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
 parser2 = EmailParser.new(emails2)
 parser2.parse
-
